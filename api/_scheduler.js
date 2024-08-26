0aaa0ce2b7c2abe25d3e30a34e226c8935de09c2
Michael Young <msyoung2012@gmail.com>
2024-08-25 20:32:26 -0400

    Update scheduler

--- api/_scheduler.js
+++ api/_scheduler.js
@@ -1,4 +1,3 @@
-// schedulerScript.js
 import cron from "node-cron";
 import { spawn } from "child_process";
 
@@ -12,15 +11,12 @@ const logWithTimestamp = (message) => {
 const runScript = (scriptPath) => {
   return new Promise((resolve, reject) => {
     const script = spawn("node", [scriptPath]);
-
     script.stdout.on("data", (data) => {
       console.log(`[${scriptPath}] ${data.toString().trim()}`);
     });
-
     script.stderr.on("data", (data) => {
       console.error(`[${scriptPath}] ${data.toString().trim()}`);
     });
-
     script.on("close", (code) => {
       if (code === 0) {
         console.log(`[${scriptPath}] Script execution completed successfully.`);
@@ -36,16 +32,17 @@ const runScript = (scriptPath) => {
 };
 
 // Schedule the scripts to run every day at 6:05 AM
-cron.schedule("04 09 * * *", async () => {
+cron.schedule("05 06 * * *", async () => {
   if (isScriptRunning) {
     logWithTimestamp("Scripts are already running. Skipping execution.");
     return;
   }
 
   isScriptRunning = true;
-  logWithTimestamp("Running scripts...");
+  logWithTimestamp("Running scheduled scripts...");
 
   try {
+    // Original scripts
     await runScript("api/arxiv/fetch-new-papers.js");
     await runScript("api/arxiv/create-embeddings.js");
     await runScript("api/arxiv/update-hn-score.js");
@@ -54,8 +51,17 @@ cron.schedule("04 09 * * *", async () => {
     await runScript("api/arxiv/generate-summary.js");
     await runScript("api/arxiv/publish-to-devto.js");
     await runScript("api/arxiv/publish-to-hashnode.js");
+
+    // New scripts
+    await runScript("api/replicate/run-all-replicate-updates.js");
+    await runScript("api/huggingFace/call-all-functions.js");
+    await runScript("api/loops/update-loops-contacts.js");
+    await runScript("api/site/topic-modeling.js");
+    await runScript("api/twitter/publish-paper-tweet.js");
+
+    logWithTimestamp("All scheduled scripts completed successfully.");
   } catch (error) {
-    logWithTimestamp(`Error running scripts: ${error}`);
+    logWithTimestamp(`Error running scheduled scripts: ${error}`);
   }
 
   isScriptRunning = false;
