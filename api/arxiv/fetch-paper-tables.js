b96fc43161e7422f1e4fc5231f12c73eb78a0f06
Michael Young <msyoung2012@gmail.com>
2024-11-30 12:28:58 -0500

    clean up headers

--- api/arxiv/fetch-paper-tables.js
+++ api/arxiv/fetch-paper-tables.js
@@ -28,17 +28,9 @@ const TYPING_SPEED = 200; // ms per character for "typing"
 const browserHeaders = {
   "User-Agent":
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-  Accept:
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+  Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
   "Accept-Language": "en-US,en;q=0.9",
-  "Accept-Encoding": "gzip, deflate, br",
   Connection: "keep-alive",
-  DNT: "1",
-  "Upgrade-Insecure-Requests": "1",
-  "Sec-Fetch-Dest": "document",
-  "Sec-Fetch-Mode": "navigate",
-  "Sec-Fetch-Site": "none",
-  "Sec-Fetch-User": "?1",
 };
 
 // Simulate human-like delay
